da5d97bfd60fa76d0518034e253de72a6379e320
JParada <jorge.parada@ctbarbanza.com>
2019-12-12 18:49:53 +0100

    .

--- app/src/main/java/cpr/castelao/aplicacinbasica/adapter/ListAdapter.java
+++ app/src/main/java/cpr/castelao/aplicacinbasica/adapter/ListAdapter.java
@@ -1,4 +1,65 @@
 package cpr.castelao.aplicacinbasica.adapter;
 
-public class ListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cpr.castelao.aplicacinbasica.R;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+
+    private static final int layout = R.layout.adapter_list_item;
+    List<String> items = new ArrayList<String>();
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(layout, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        String item = items.get(position);
+
+        holder.txtTitle.setText(item);
+        holder.txtSubtitle.setText("POS: "+position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtTitle, txtSubtitle;
+        public ImageView image;
+
+        public LinearLayout linRoot, linContent;
+
+        void initView(View v){
+            txtTitle =  v.findViewById(R.id.adapter_list_item_title_txt);
+            txtSubtitle = v.findViewById(R.id.adapter_list_item_subtitle_txt);
+            image = v.findViewById(R.id.adapter_list_item_img);
+            linRoot = v.findViewById(R.id.adapter_list_item_root_lin);
+            linContent = v.findViewById(R.id.adapter_list_item_content_lin);
+        }
+
+        public MyViewHolder(View v) {
+            super(v);
+            initView(v);
+        }
+    }
 }
