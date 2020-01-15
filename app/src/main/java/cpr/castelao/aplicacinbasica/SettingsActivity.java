b4680fe9099ee8149d60a6363f6c447e59a966e3
DoberWho <jorge.parada@ctbarbanza.com>
2020-01-15 13:08:04 +0100

    Crear colores personalizados y usarlos en los Botones (O Cualquier otro Widget) =>NOTA: Se recoge un int desde el resources; y es el que debemos usar.

--- app/src/main/java/cpr/castelao/aplicacinbasica/SettingsActivity.java
+++ app/src/main/java/cpr/castelao/aplicacinbasica/SettingsActivity.java
@@ -2,6 +2,7 @@ package cpr.castelao.aplicacinbasica;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -25,6 +26,11 @@ public class SettingsActivity extends BasicApp {
         String ip = getString(R.string.IPSERVIDOR);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setBackgroundResource(R.color.naranjito);
+        int colorint = getColor(R.color.naranjito);
+        fab.setBackgroundColor(colorint);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
