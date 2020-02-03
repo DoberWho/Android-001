34b8426c15cfe2225a5793d30a92801930cc0d19
DoberWho <jorge.parada@ctbarbanza.com>
2020-02-03 14:03:22 +0100

    => Niveles de LOG - Es importante tener un Log coherente y usar los niveles de log para lo que se deben usar; de forma que no expongamos datos criticos de nuestra aplicación fuera de una capa que no sea relevante; o que pueda generar un error de seguridad a la hora de pasar a producción
    => NOTA: System.out está en capa de android; no se puede capar en la
    compilación del APK
    => NOTA: En el momento de generar el APK, el compilador se carga los
    LOGs que no sean de Warning o Error.

--- app/src/main/java/cpr/castelao/aplicacinbasica/MainActivity.java
+++ app/src/main/java/cpr/castelao/aplicacinbasica/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -54,6 +55,13 @@ public class MainActivity extends BasicApp {
         setContentView(R.layout.activity_main);
         Realm.init(this);
         toast("onCreate");
+
+        System.out.println("HOLA ANDROID");
+        Log.v("MAIN","Hola Verbose");
+        Log.i("MAIN","Hola Info");
+        Log.d("MAIN","Hola Debug");
+        Log.w("MAIN","Hola Warning");
+        Log.e("MAIN","Hola Error");
 
 
         initButtons();
